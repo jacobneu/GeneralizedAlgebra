a3cc3ec3977111292da3fcebe13b99fac257882a
Jacob Neumann <jacobneumann21@gmail.com>
2025-06-23 19:18:26 +0000

    Implemented elim for quiver

--- GeneralizedAlgebra/signatures/quiver.lean
+++ GeneralizedAlgebra/signatures/quiver.lean
@@ -1,6 +1,8 @@
-import GeneralizedAlgebra.nouGAT
+import GeneralizedAlgebra.signatures.set
 
-def 𝔔𝔲𝔦𝔳 : GAT := ⦃
+def 𝔔𝔲𝔦𝔳 : GAT := ⟨ ⦃
     V : U,
     E : V ⇒ V ⇒ U
-⦄
+⦄,
+λ P => P.cons_D _ (𝔖𝔢𝔱.elim P) _ (P.PI_D _ _ _ (P.VAR0_D _ _ _ _ _) _ (P.PI_D _ _ _ (P.VARSUCC_D _ _ Ty.UU (P.UU_D _ _) _ (P.VAR0_D _ _ _ _ _) _ _ _) _ (P.UU_D _ _)))
+⟩
