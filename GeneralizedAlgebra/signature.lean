4fdceab14b304d88e3cff8f46fd30665c4d5159b
Jacob Neumann <jacobneumann21@gmail.com>
2026-02-11 16:02:11 +0000

    New attempt at typechecking

--- GeneralizedAlgebra/signature.lean
+++ GeneralizedAlgebra/signature.lean
@@ -23,14 +23,38 @@ instance : GetElem preCon Nat preTy fun (Γ : preCon) (i : Nat) => i < Γ.length
 def preEXTEND (Γ : preCon) (A : preTy) := A :: Γ
 def preEMPTY : preCon := []
 
+def preWkTmArr : Nat → preTm → preTm
+| a, preVAR n => if n < a then preVAR n else preVAR (succ n)
+| a, preAPP f t => preAPP (preWkTmArr a f) (preWkTmArr a t)
+| a, preTRANSP eq t => preTRANSP (preWkTmArr a eq) (preWkTmArr a t)
 
-def preWkTm : preTm → preTm
-| preVAR n => preVAR (succ n)
-| preAPP f t => preAPP (preWkTm f) (preWkTm t)
-| preTRANSP eq t => preTRANSP (preWkTm eq) (preWkTm t)
+def preWkTyArr : Nat → preTy → preTy
+| _, preUU => preUU
+| a, preEL t => preEL (preWkTmArr a t)
+| a, preEQ s t => preEQ (preWkTmArr a s) (preWkTmArr a t)
+| a, prePI X Y => prePI (preWkTmArr a X) (preWkTyArr (succ a) Y)
+
+def preWkTm := preWkTmArr 0
+def preWkTy := preWkTyArr 0
+
+
+def substTm : Nat → preTm → preTm → preTm
+| a, s, preVAR n =>
+  match compare n a with
+    | Ordering.lt => preVAR n
+    | Ordering.eq => s
+    | Ordering.gt => preVAR (n - 1)
+| a, s, preAPP f t => preAPP (substTm a s f) (substTm a s t)
+| a, s, preTRANSP eq t => preTRANSP (substTm a s eq) (substTm a s t)
+
+def substTy : Nat → preTm → preTy → preTy
+| _, _, preUU => preUU
+| a, s, preEL t => preEL (substTm a s t)
+| a, s, preEQ s' t => preEQ (substTm a s s') (substTm a s t)
+| a, s, prePI X Y => prePI (substTm a s X) (substTy (succ a) s Y)
 
 inductive preArg : Type where
-| preImpl : String → preTy → preArg
+-- | preImpl : String → preTy → preArg
 | preExpl : String → preTy → preArg
 | preAnon : preTy → preArg
 open preArg
--- GeneralizedAlgebra/typecheck.lean
+++ GeneralizedAlgebra/typecheck.lean
@@ -6,253 +6,141 @@ open Nat Except
 open preTy preTm
 
 mutual
-inductive Con : Type where
-| EMPTY : Con
-| EXTEND : Con → Ty → Con
 
-inductive Ty : Type where
-| UU : Ty
-| EL : Tm → Ty
-| PI : Tm → Ty → Ty
+inductive wellTy : preCon → preTy → Type where
+| wellUU : ∀ {Γ : preCon}, wellTy Γ preUU
+| wellWkTy : ∀ {Γ : preCon} {A : preTy} {B : preTy}, wellTy Γ A → wellTy (B :: Γ) (preWkTy A)
+| wellEL : ∀ {Γ : preCon} {X : preTm}, wellTm Γ preUU X → wellTy Γ (preEL X)
+| wellPI : ∀ {Γ : preCon} {X : preTm} {Y : preTy}, wellTm Γ preUU X → wellTy (preEL X :: Γ) Y → wellTy Γ (prePI X Y)
 
-inductive Tm : Type where
-| VAR : Nat → Tm
-| APP : Tm → Ty → Tm → Tm → Tm
+inductive wellTm : preCon → preTy → preTm → Type where
+| wellZero : ∀ {Γ : preCon}{A : preTy}, wellTy Γ A → wellTm (A :: Γ) (preWkTy A) (preVAR 0)
+| wellWkTm : ∀ {Γ : preCon}{A B : preTy}{t : preTm}, wellTm Γ A t → wellTm (B :: Γ) (preWkTy A) (preWkTm t)
+| wellAPP : ∀ {Γ : preCon} {X : preTm} {Y : preTy} {f s : preTm}, wellTm Γ (prePI X Y) f → wellTm Γ (preEL X) s → wellTm Γ (substTy 0 s Y) (preAPP f s)
 
-end
-open Con Ty Tm
-
-mutual
-def Ty.repr : Ty → String
-| UU => "U"
-| EL X => "EL (" ++ (Tm.repr X) ++ ")"
-| PI X Y => "Π (" ++ (Tm.repr X) ++ ") (" ++ (Ty.repr Y) ++ ")"
-
-def Tm.repr : Tm → String
-| VAR n => Nat.repr n
-| APP _ _ f t => (Tm.repr f) ++ " @ " ++ (Tm.repr t)
-end
-
-mutual
-def Tm.decEq (t t' : Tm) : Decidable (t = t') :=
-  match t, t' with
-  | VAR m , VAR n => match Nat.decEq m n with
-      | isTrue h => isTrue (congrArg VAR h)
-      | isFalse h => isFalse (fun h' => by injection h' with e; exact h e)
-  | APP X Y f t, APP X' Y' f' t' =>
-      match Tm.decEq X X', Ty.decEq Y Y', Tm.decEq f f', Tm.decEq t t' with
-      | isTrue hX, isTrue hY, isTrue hf, isTrue ht =>
-          isTrue $ congr (congr (congr (congrArg _ hX) hY) hf) ht
-      | isFalse hX, _, _, _ => isFalse (fun h' => by injection h' with eX; exact hX eX)
-      | _, isFalse hY, _, _ => isFalse (fun h' => by injection h' with eX eY; exact hY eY)
-      | _, _, isFalse hf, _ => isFalse (fun h' => by injection h' with eX eY ef; exact hf ef)
-      | _, _, _, isFalse ht => isFalse (fun h' => by injection h' with eX eY ef et; exact ht et)
-  | APP _ _ _ _, VAR _ => isFalse (fun h => Tm.noConfusion h)
-  | VAR _, APP _ _ _ _ => isFalse (fun h => Tm.noConfusion h)
-
-def Ty.decEq (A B : Ty) : Decidable (A = B) :=
-  match A, B with
-  | UU , UU => isTrue rfl
-  | UU , EL _ => isFalse (fun h => Ty.noConfusion h)
-  | UU , PI _ _ => isFalse (fun h => Ty.noConfusion h)
-  | EL _ , UU => isFalse (fun h => Ty.noConfusion h)
-  | EL t , EL t' => match Tm.decEq t t' with
-      | isTrue h => isTrue (congrArg EL h)
-      | isFalse h => isFalse (fun h' => by injection h' with e; exact h e)
-  | EL _ , PI _ _ => isFalse (fun h => Ty.noConfusion h)
-  | PI _ _, UU => isFalse (fun h => Ty.noConfusion h)
-  | PI _ _, EL _ => isFalse (fun h => Ty.noConfusion h)
-  | PI X Y, PI X' Y' => match Tm.decEq X X', Ty.decEq Y Y' with
-      | isTrue hX, isTrue hY => isTrue (congr (congrArg _ hX) hY)
-      | isFalse hX, _ => isFalse (fun h' => by injection h' with e; exact hX e)
-      | _ , isFalse hY => isFalse (fun h' => by injection h' with e e'; exact hY e')
-end
-
-instance : DecidableEq Tm := Tm.decEq
-instance : DecidableEq Ty := Ty.decEq
-
-mutual
-inductive probTy : Type where
-| litTy : Ty → probTy
-| QTy : probTy
-| probEL : probTm → probTy
-| probPI : probTm → probTy → probTy
-
-inductive probTm : Type where
-| litTm : Tm → probTm
-| QTm : probTm
-| probVAR : probTm
-
-end
-open probTy probTm
-
-mutual
-def wkTy : Ty → Ty
-| UU => UU
-| PI X Y => PI (wkTm X) (wkTy Y)
-| EL X => EL (wkTm X)
-def wkTm : Tm → Tm
-| VAR n => VAR (succ n)
-| APP X Y f t => APP (wkTm X) (wkTy Y) (wkTm f) (wkTm t)
-def unwkTy : Ty → Except String Ty
-| UU => return UU
-| PI X Y => do
-    let X' ← unwkTm X
-    let Y' ← unwkTy Y
-    return PI X' Y'
-| EL X => do
-    let X' ← unwkTm X
-    return EL X'
-def unwkTm : Tm → Except String Tm
-| VAR 0 => error "Tried to unweaken VAR 0"
-| APP X Y f t => do
-    let X' ← unwkTm X
-    let Y' ← unwkTy Y
-    let f' ← unwkTm f
-    let t' ← unwkTm t
-    return APP X' Y' f' t'
-| VAR (succ n) => return VAR n
-
-def punwkTy : probTy → Except String probTy
-| litTy A => do
-    let A' ← unwkTy A
-    return litTy A'
-| probPI pX pY => do
-    let pX' ← punwkTm pX
-    let pY' ← punwkTy pY
-    return probPI pX' pY'
-| probEL pX => do
-    let pX' ← punwkTm pX
-    return probEL pX'
-| QTy => return QTy
-
-def punwkTm : probTm → Except String probTm
-| litTm X => do
-    let X' ← unwkTm X
-    return litTm X'
-| probVAR => return probVAR
-| QTm => return QTm
-end
-
-mutual
-def redTy : probTy → probTy
-| probEL pX => match redTm pX with
-  | litTm X => litTy (EL X)
-  | pX' => probEL pX'
-| probPI pX pY => match redTm pX, redTy pY with
-  | litTm X, litTy Y => litTy (PI X Y)
-  | pX', pY' => probPI pX' pY'
-| Z => Z
-
-def redTm : probTm → probTm
-| Z => Z
-end
-
-mutual
-def matchesTy (pA : probTy) (B : Ty) : Except String Ty :=
-  match redTy pA , B with
-  | QTy, _ => return B
-  | litTy A, _ => if A = B then return A else error $ "literal type `" ++ Ty.repr A ++ "` does not match `" ++ (Ty.repr B) ++ "`"
-  | probPI pX pY, PI X Y => do
-      let _ ← matchesTm pX X
-      let _ ← matchesTy pY Y
-      return (PI X Y)
-  | probEL pX, EL X => do
-      let _ ← matchesTm pX X
-      return (EL X)
-  | _, _ => error $ "Wildcard matchesTy"
-
-def matchesTm (pX : probTm) (X : Tm) : Except String Tm :=
-  match redTm pX, X with
-  | QTm , _ => return X
-  | litTm Z, _ => if Z = X then return X else error $ "literal term `" ++ Tm.repr Z ++ "` does not match `" ++ Tm.repr X ++ "`"
-  | _ , _ => error $ "Wildcard matchesTm"
-end
-
-def extrPI (pPIXY : probTy) : Except String (probTm × probTy) := match redTy pPIXY with
-  | litTy (PI X Y) => return (litTm X,litTy Y)
-  | QTy =>  error $ "Wildcard extrPI QTy"
-  | probPI pX pY => return (redTm pX, redTy pY)
-  | _ =>  error $ "Wildcard extrPI"
-
-def extrEL (pELX : probTy) : Except String probTm := match redTy pELX with
-  | litTy (EL X) => return litTm X
-  | QTy =>  error $ "Wildcard extrEL QTy"
-  | probEL pX => return pX
-  | _ =>  error $ "Wildcard extrEL"
-
-def forceTm : probTm → Except String Tm
-| litTm X => return X
-| _ => error "Failed to extract Tm"
-def forceTy : probTy → Except String Ty
-| litTy A => return A
-| _ => error "Failed to extract Ty"
-
-mutual
-def compileCon : preCon → Except String Con
-| [] => return EMPTY
-| A::AS => do
-    let Γ ← compileCon AS
-    let cA ← compileTy Γ A
-    return EXTEND Γ cA
-
-def compileTy : Con → preTy → Except String Ty
-| _ , preUU => return UU
-| Γ , preEL X => do
-    let (cX,_) ← compileTm Γ (litTy UU) X
-    return EL cX
-| Γ , prePI X Y => do
-    let (cX,_) ← compileTm Γ (litTy UU) X
-    let cY ← compileTy (EXTEND Γ (EL cX)) Y
-    return PI cX cY
-| _ , _ => error $ "Wildcard compileTy"
-
-def compileTm : Con → probTy → preTm → Except String (Tm × probTy)
-| EXTEND _ A, pA , preVAR 0 => do
-    let _ ← matchesTy pA A
-    return (VAR 0, litTy (wkTy A))
-| EXTEND Γ _, pA , preVAR (succ n) => do
-  let pA' ← punwkTy pA
-  match compileTm Γ pA' (preVAR n) with
-  | ok (VAR k, A) => return (VAR (succ k), A)
-  | error e =>  error $ "Wildcard EXTEND Γ _, pA, preVAR (succ n): error '" ++ e ++ "'"
-  | _ =>  error $ "Wildcard EXTEND Γ _, pA, preVAR (succ n)"
-| Γ , Yt, preAPP f t => do
-    let (cf,pPIXY) ← compileTm Γ (probPI QTm QTy) f
-    let (pX,pY) ← extrPI pPIXY
-    -- let (ct,pELX) ← compileTm Γ (QTy) t
-    let (ct,pELX) ← compileTm Γ (redTy (probEL pX)) t
-    let pX' ← extrEL pELX
-    let X ← forceTm (redTm pX')
-    let Y ← forceTy (redTy pY)
-    let _ ← matchesTy pELX (EL X)
-    -- Check Yt
-    return (APP X Y cf ct, Yt)
-| _ , _ , _ =>  error $ "Wildcard compileTm"
 
 end
 
+inductive wellCon : preCon → Type where
+| wellEmpty : wellCon []
+| wellCons : ∀ {Γ : preCon}{A : preTy}, wellTy Γ A → wellCon Γ → wellCon (A :: Γ)
 
-structure GAT extends GATdata where
-  (typedCon : Except String Con)
+open wellTy wellTm wellCon
+
+
+
+
+-- mutual
+
+-- def synthTy : preTy → Con → Ty
+-- | preUU , _ => UU
+-- |
+
+
+-- end
+
+
+-- structure GAT extends GATdata where
+--   (typedCon : Except String Con)
 --   (elim : (P : indData) → P.Con_D con)
 
 
-def getSuccess {α} : Except String α → Option α
-| Except.ok x => some x
-| Except.error _ => none
-def getError {α} : Except String α → Option String
-| Except.ok _ => none
-| Except.error s => some s
+-- def getSuccess {α} : Except String α → Option α
+-- | Except.ok x => some x
+-- | Except.error _ => none
+-- def getError {α} : Except String α → Option String
+-- | Except.ok _ => none
+-- | Except.error s => some s
+
+def test0 := [
+  preEL (preVAR 0),
+  preUU]
+
+def thm0 : wellCon test0 :=
+by
+    apply wellCons
+    apply wellEL
+    apply wellZero
+    apply wellUU
+    apply wellCons
+    apply wellUU
+    apply wellEmpty
 
 def test1 := [
-  -- prePI (preVAR 1) (preEL $ preAPP (preVAR 1) (preVAR 0)) ,
---   preEL $ preAPP (preVAR 0) (preVAR 1),
   prePI (preVAR 1) preUU,
   preEL (preVAR 0),
   preUU]
-#eval getError $ compileCon test1
-#reduce compileCon test1
+
+def thm1 : wellCon test1 :=
+by
+    apply wellCons
+    apply wellPI
+    apply @wellWkTm [preUU] preUU _ (preVAR 0)
+    apply wellZero
+    apply wellUU
+    apply wellUU
+    apply thm0
+
+def test2 := [
+  -- prePI (preVAR 1) (preEL $ preAPP (preVAR 1) (preVAR 0)) ,
+  preEL $ preAPP (preVAR 0) (preVAR 1),
+  prePI (preVAR 1) preUU,
+  preEL (preVAR 0),
+  preUU]
+
+def thm2 : wellCon test2 :=
+by
+    apply wellCons
+    apply wellEL
+    apply @wellAPP _ (preVAR 2) preUU _ _
+    apply wellZero
+    apply wellPI
+    apply @wellWkTm _ preUU (preEL (preVAR 0)) (preVAR 0)
+    apply wellZero
+    apply wellUU
+    apply wellUU
+    apply @wellWkTm _ (preEL (preVAR 1)) _ (preVAR 0)
+    apply wellZero
+    apply wellEL
+    apply wellZero
+    apply wellUU
+    exact thm1
+
+def test3 := [
+  prePI (preVAR 2) (preEL $ preAPP (preVAR 1) (preVAR 0)),
+  prePI (preVAR 1) preUU,
+  preEL (preVAR 0),
+  preUU]
+
+#check wellWkTm
+#check wellAPP
+
+def thm3 : wellCon test3 :=
+by
+  apply wellCons
+  apply wellPI
+  apply @wellWkTm _ preUU _ (preVAR 1)
+  apply @wellWkTm _ preUU _ (preVAR 0)
+  apply wellZero
+  apply wellUU
+  apply wellEL
+  apply @wellAPP _ (preVAR 3) preUU _ _
+  apply @wellWkTm _ (prePI (preVAR 2) preUU) _ (preVAR 0)
+  apply wellZero
+  apply wellPI
+  apply @wellWkTm _ preUU _ (preVAR 0)
+  apply wellZero
+  apply wellUU
+  apply wellUU
+  apply wellZero
+  apply wellEL
+  apply @wellWkTm _ preUU _ (preVAR 1)
+  apply @wellWkTm _ preUU _ (preVAR 0)
+  apply wellZero
+  apply wellUU
+  apply thm1
+-- #eval getError $ compileCon test1
+-- #reduce compileCon test1
 
 -- #reduce getSuccess $ do
 --     let Γ ← compileCon test1
@@ -267,10 +155,10 @@ def test := [
   preEL (preVAR 0),
   preUU]
 
-#reduce getError $ do
-    let Γ ← compileCon test
-    -- compileTm Γ (litTy UU) (preAPP (preVAR 0) (preVAR 1))
-    compileTm Γ (litTy UU) (preAPP (preAPP (preVAR 0) (preVAR 1)) (preVAR 1))
+-- #reduce getError $ do
+--     let Γ ← compileCon test
+--     -- compileTm Γ (litTy UU) (preAPP (preVAR 0) (preVAR 1))
+--     compileTm Γ (litTy UU) (preAPP (preAPP (preVAR 0) (preVAR 1)) (preVAR 1))
 
-#reduce compileCon test
-#eval getError $ compileCon test
+-- #reduce compileCon test
+-- #eval getError $ compileCon test
