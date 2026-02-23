e36c9e76358f7928b04eb5bcc18ffc2bdaf7a065
Jacob Neumann <jacobneumann21@gmail.com>
2026-02-23 14:51:02 +0000

    rm deadcode from helper.lean

--- GeneralizedAlgebra/helper.lean
+++ GeneralizedAlgebra/helper.lean
@@ -1,25 +1,6 @@
 
 open Nat
 
-def stringCons := @List.cons String
-def stringNil  := @List.nil String
-def stringPure := @List.pure String
-
-
-def snoc (L : List String) x := L ++ [x]
-
-def filterNilStr := List.filter (Bool.not ∘ String.isEmpty)
-
-def mappartial {α β} (f : α → Option β) : List α → List β
-| [] => []
-| x::xs => match f x with
-  | some y => y::mappartial f xs
-  | none => mappartial f xs
-
-def mk2 (x:Con) (y:List String):= (x,y)
-def mk3 {α : Type} (x:Con) (y:List String) (z : α) := (x,y,z)
-
-
 def stripFirstParen : List Char → Option (List Char)
 | '('::xs => some xs
 | _ => none
@@ -47,28 +28,3 @@ def parensUnnecessary (s : String) : Bool :=
 || (Option.isNone $ List.find? Char.isWhitespace $ String.toList s)
 
 def paren (s:String):String := if parensUnnecessary s then s else "("++s++")"
-
-
-def parenX (s:String):String :=
-  if String.isNat (String.drop s 2) then s else "("++s++")"
-
-
-def nth {α : Type}: Nat → List α → Option α
-| _, [] => none
-| 0, x::_ => return x
-| succ n, _::xs => nth n xs
-
-def nthBackwards (n:Nat) (L : List Nat) : Option Nat := nth n (List.reverse L)
-
-def twoRepr (n : Nat) : String :=
-  if n<10 then "0"++ Nat.repr n else
-  Nat.repr n
-def threeRepr (n : Nat) : String :=
-  if n<10 then "00"++ Nat.repr n else
-  if n<100 then "0"++ Nat.repr n else
-  Nat.repr n
-
-def collapse : List String → String
-| [] => ""
-| [x] => x
-| x::L => "(" ++  List.foldl (λ s s' => s ++ "," ++ s' ) x L ++   ")"
