d0a9fe14ffd3fbf8c85ea952ea96818d7024a954
Jacob Neumann <jacobneumann21@gmail.com>
2026-03-20 01:09:20 +0000

    Commented out wellCon proofs which broke for some reason

--- GeneralizedAlgebra/signatures/nat.lean
+++ GeneralizedAlgebra/signatures/nat.lean
@@ -6,18 +6,18 @@ def 𝔑_data : GATdata := [GATdata|
     s  : N ⇒ N
 ]
 
-def 𝔑 : GAT := ⟨
-    𝔑_data,
-    by
-        apply wellCon.wellCons
-        apply wellTy.wellPI
-        apply @wellTm.wellWkTm _ preTy.preUU
-        apply wellTm.wellZero
-        apply wellTy.wellUU
-        apply wellTy.wellEL
-        apply @wellTm.wellWkTm _ preTy.preUU
-        apply @wellTm.wellWkTm _ preTy.preUU
-        apply wellTm.wellZero
-        apply wellTy.wellUU
-        exact 𝔓.2
-⟩
+-- def 𝔑 : GAT := ⟨
+--     𝔑_data,
+--     by
+--         apply wellCon.wellCons
+--         apply wellTy.wellPI
+--         apply @wellTm.wellWkTm _ preTy.preUU
+--         apply wellTm.wellZero
+--         apply wellTy.wellUU
+--         apply wellTy.wellEL
+--         apply @wellTm.wellWkTm _ preTy.preUU
+--         apply @wellTm.wellWkTm _ preTy.preUU
+--         apply wellTm.wellZero
+--         apply wellTy.wellUU
+--         exact 𝔓.2
+-- ⟩
