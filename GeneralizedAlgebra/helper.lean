b2ea1e7e2845501ed39fd36595e02851c5c54b84
Jacob Neumann <jacobneumann21@gmail.com>
2026-03-27 14:48:25 +0000

    Cleaned up some comments, implemented rawMode boolean to clean up output

--- GeneralizedAlgebra/helper.lean
+++ GeneralizedAlgebra/helper.lean
@@ -27,12 +27,4 @@ def parensUnnecessary (s : String) : Bool :=
    Option.isSome (stripOuterParen s >>= List.foldl parenCounter (some 0))
 || (Option.isNone $ List.find? Char.isWhitespace $ String.toList s)
 
-def paren (s:String):String := if parensUnnecessary s then s else "("++s++")"
-
-def replaceAllWithSpace (asSpace : List String) (s : String) :String :=
-  List.foldl (λ as curr => curr.replace as " ") s asSpace
-
-def paren' (sep : String) (sl : List String) (asSpace : List String := []): String :=
-  if parensUnnecessary (String.intercalate " " (List.map (replaceAllWithSpace asSpace) sl))
-  then String.intercalate sep sl
-  else "("++ String.intercalate sep sl ++")"
+def mkParen (s:String):String := if parensUnnecessary s then s else "("++s++")"
