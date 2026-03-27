8e5a45584191cd01f2b244baebabec6a968672d0
Jacob Neumann <jacobneumann21@gmail.com>
2026-03-27 23:07:07 +0000

    Started implementing holes

--- GeneralizedAlgebra/eliminate/ConPrinting.lean
+++ GeneralizedAlgebra/eliminate/ConPrinting.lean
@@ -17,6 +17,7 @@ def preTmrepr : preTm → String
 | preAPP f t =>   mkParen (preTmrepr f) ++ " @ " ++ mkParen (preTmrepr t)
 | preVAR n => Nat.repr n
 | preTRANSP eq y => "transp " ++ mkParen (preTmrepr eq) ++ " " ++ mkParen (preTmrepr y)
+| holeTm => "?"
 
 def preTyrepr : preTy → String
 | preUU => "U"
