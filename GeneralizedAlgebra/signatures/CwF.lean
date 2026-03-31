4bae036e4ba50e37c884409b95054345176f502d
Jacob Neumann <jacobneumann21@gmail.com>
2026-03-31 14:45:41 +0000

    Added syntax for bunching arguments

--- GeneralizedAlgebra/signatures/CwF.lean
+++ GeneralizedAlgebra/signatures/CwF.lean
@@ -3,47 +3,42 @@ import GeneralizedAlgebra.nouGAT_eliminate
 def ℭ𝔴𝔉_data  := [GATdata|
     Con : U,
     Sub : Con ⇒ Con ⇒ U,
-    id  : ( Γ : Con) ⇒ Sub Γ Γ,
-    comp  : (Θ :Con) ⇒ (Δ : Con) ⇒ (Γ : Con) ⇒
-            Sub Δ Γ ⇒ Sub Θ Δ ⇒ Sub Θ Γ,
-    lunit : (Δ : Con) ⇒ (Γ : Con) ⇒ ( γ : Sub Δ Γ) ⇒
+    id  : (Γ : Con) ⇒ Sub Γ Γ,
+    comp  : (Θ Δ Γ : Con) ⇒ Sub Δ Γ ⇒ Sub Θ Δ ⇒ Sub Θ Γ,
+    lunit : (Δ Γ : Con) ⇒ (γ : Sub Δ Γ) ⇒
             comp Δ Γ Γ (id Γ) γ ≡ γ,
-    runit : (Δ : Con) ⇒ (Γ : Con) ⇒ ( γ : Sub Δ Γ) ⇒
+    runit : (Δ Γ : Con) ⇒ (γ : Sub Δ Γ) ⇒
             comp Δ Δ Γ γ (id Δ) ≡ γ,
-    assoc : (Ξ:Con) ⇒ (Θ:Con) ⇒ (Δ:Con) ⇒ (Γ:Con) ⇒ (ϑ : Sub Ξ Θ) ⇒
-            (δ : Sub Θ Δ) ⇒ (γ : Sub Δ Γ) ⇒
+    assoc : (Ξ Θ Δ Γ:Con) ⇒ (ϑ : Sub Ξ Θ) ⇒ (δ : Sub Θ Δ) ⇒ (γ : Sub Δ Γ) ⇒
             comp Ξ Δ Γ γ (comp Ξ Θ Δ δ ϑ) ≡ comp Ξ Θ Γ (comp Θ Δ Γ γ δ) ϑ,
     empty : Con,
     ε : (Γ : Con) ⇒ Sub Γ empty,
     ηε : (Γ : Con) ⇒ (f : Sub Γ empty) ⇒ f ≡ (ε Γ),
     Ty      : Con ⇒ U,
-    substTy : (Δ:Con)⇒ (Γ : Con) ⇒ Sub Δ Γ ⇒ Ty Γ ⇒ Ty Δ,
+    substTy : (Δ Γ : Con) ⇒ Sub Δ Γ ⇒ Ty Γ ⇒ Ty Δ,
     idTy    : (Γ : Con) ⇒ (A : Ty Γ) ⇒
               substTy Γ Γ (id Γ) A ≡ A,
-    compTy  : (Θ:Con)⇒ (Δ:Con)⇒  (Γ : Con) ⇒ (A : Ty Γ) ⇒
+    compTy  : (Θ Δ Γ : Con) ⇒ (A : Ty Γ) ⇒
               (γ : Sub Δ Γ) ⇒ (δ : Sub Θ Δ) ⇒
               substTy Θ Δ δ (substTy Δ Γ γ A)
               ≡ substTy Θ Γ (comp Θ Δ Γ γ δ) A,
     Tm      : (Γ : Con) ⇒ Ty Γ ⇒ U,
-    substTm : (Δ:Con)⇒ (Γ : Con) ⇒ (A : Ty Γ) ⇒
+    substTm : (Δ Γ : Con) ⇒ (A : Ty Γ) ⇒
               (γ : Sub Δ Γ) ⇒
               Tm Γ A ⇒ Tm Δ (substTy Δ Γ γ A),
-    idTm    : ( Γ : Con) ⇒ (A : Ty Γ) ⇒ (t : Tm Γ A) ⇒
+    idTm    : (Γ : Con) ⇒ (A : Ty Γ) ⇒ (t : Tm Γ A) ⇒
               substTm Γ Γ A (id Γ) t    #⟨idTy Γ A⟩
               ≡ t,
-    compTm  : (Θ:Con)⇒ (Δ:Con)⇒ (Γ : Con) ⇒
-              (A : Ty Γ) ⇒ (t : Tm Γ A) ⇒
+    compTm  : (Θ Δ Γ : Con) ⇒ (A : Ty Γ) ⇒ (t : Tm Γ A) ⇒
               (γ : Sub Δ Γ) ⇒ (δ : Sub Θ Δ) ⇒
               substTm Θ Δ (substTy Δ Γ γ A) δ
                 (substTm Δ Γ A γ t)    #⟨compTy Θ Δ Γ A γ δ⟩
               ≡ substTm Θ Γ A (comp Θ Δ Γ γ δ) t,
-    ext     : ( Γ : Con) ⇒ Ty Γ ⇒ Con,
-    pair    : (Δ:Con)⇒ (Γ : Con) ⇒ (A : Ty Γ) ⇒
-              (γ : Sub Δ Γ) ⇒
+    ext     : (Γ : Con) ⇒ Ty Γ ⇒ Con,
+    pair    : (Δ Γ : Con) ⇒ (A : Ty Γ) ⇒ (γ : Sub Δ Γ) ⇒
               Tm Δ (substTy Δ Γ γ A) ⇒
               Sub Δ (ext Γ A),
-    pair_nat: (Θ:Con)⇒ (Δ:Con)⇒ (Γ : Con) ⇒ (A : Ty Γ) ⇒
-              (γ : Sub Δ Γ) ⇒
+    pair_nat: (Θ Δ Γ : Con) ⇒ (A : Ty Γ) ⇒ (γ : Sub Δ Γ) ⇒
               (t : Tm Δ (substTy Δ Γ γ A)) ⇒
               (δ : Sub Θ Δ) ⇒
               comp Θ Δ (ext Γ A) (pair Δ Γ A γ t) δ
@@ -51,11 +46,11 @@ def ℭ𝔴𝔉_data  := [GATdata|
     p      : (Γ : Con) ⇒ (A : Ty Γ) ⇒ Sub (ext Γ A) Γ,
     v      : (Γ : Con) ⇒ (A : Ty Γ) ⇒
               Tm (ext Γ A) (substTy (ext Γ A) Γ (p Γ A) A),
-    ext_β₁  : (Δ:Con)⇒ (Γ : Con) ⇒ (A : Ty Γ) ⇒
+    ext_β₁  : (Δ Γ : Con) ⇒ (A : Ty Γ) ⇒
               (γ : Sub Δ Γ) ⇒
               (t : Tm Δ (substTy Δ Γ γ A)) ⇒
               comp Δ (ext Γ A) Γ (p Γ A) (pair Δ Γ A γ t) ≡ γ,
-    ext_β₂  : (Δ:Con)⇒ (Γ : Con) ⇒ (A : Ty Γ) ⇒
+    ext_β₂  : (Δ Γ : Con) ⇒ (A : Ty Γ) ⇒
               (γ : Sub Δ Γ) ⇒ (t : Tm Δ (substTy Δ Γ γ A)) ⇒
               substTm Δ (ext Γ A) (substTy (ext Γ A) Γ (p Γ A) A) (pair Δ Γ A γ t) (v Γ A)
                   #⟨compTy Δ (ext Γ A) Γ A (p Γ A) (pair Δ Γ A γ t)⟩  #⟨ext_β₁ Δ Γ A γ t⟩
--- GeneralizedAlgebra/signatures/setoid.lean
+++ GeneralizedAlgebra/signatures/setoid.lean
@@ -3,11 +3,8 @@ import GeneralizedAlgebra.nouGAT_eliminate
 def 𝔖𝔢𝔱𝔬𝔦𝔡_data : GATdata := [GATdata|
     X : U,
     eq : X ⇒ X ⇒ U,
-    eqη : (x : X) ⇒ (x' : X) ⇒
-        (p : eq x x') ⇒ (q : eq x x') ⇒ p ≡ q,
+    eqη : (x x' : X) ⇒ (p q : eq x x') ⇒ p ≡ q,
     rfl : (x : X) ⇒ eq x x,
-    sym : (x : X) ⇒ (y : X) ⇒
-        eq x y ⇒ eq y x,
-    trns : (x : X) ⇒ (y : X) ⇒ (z : X) ⇒
-        eq x y ⇒ eq y z ⇒ eq x z
+    sym : (x y : X) ⇒ eq x y ⇒ eq y x,
+    trns : (x y z : X) ⇒ eq x y ⇒ eq y z ⇒ eq x z
 ]
