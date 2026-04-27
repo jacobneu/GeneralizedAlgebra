6f7208e9afa01fb3d49fe1adfabda1af4ed6f3f6
Jacob Neumann <jacobneumann21@gmail.com>
2026-04-27 18:55:48 +0000

    Got forester working; used in 2026-04-27 version of genalg forester builds

--- GeneralizedAlgebra/eliminate/formats/Forester.lean
+++ GeneralizedAlgebra/eliminate/formats/Forester.lean
@@ -39,12 +39,19 @@ def forFormatLine (s : String) : sfDecor → String
 | _ => liFormat s
 -- | _ => "   " ++ s
 
+def inlineAlgFmt : (String → String) → List String → String
+| _,[] => ""
+| f,[x] => "#{" ++ f x ++ "}"
+| f,[x,y] => "#{(" ++ f x ++ ", " ++ f y ++ ")}"
+| f,[x,y,z] => "#{(" ++ f x ++ ", " ++ f y ++ ", " ++ f z ++ ")}"
+| f,l => "#{(" ++ String.intercalate ", " (List.map f (l.take 3)) ++ ", \\ldots)}"
+
 def forFormatWrapping (G _ : String) (topnames : List String) : sfDecor → List String × List String
 | sfId => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\mathfrak{" ++ G ++ "}}}","\\p{The GAT #{\\GAT{" ++ G ++ "}} is given by the signature","\\<html:ul>[style]{list-style-type:'▷  ';list-style-position: inside;}{","\\<html:li>[style]{list-style-type:'◇';list-style-position: outside;}{}"],["}","}"])
 | sfAlg => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Alg{" ++ G ++ "}}}","\\p{A \\strong{\\GAT{" ++ G ++ "}-algebra} consists of","\\ul{"],["}","}"])
-| sfDalg => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\DAlg{" ++ G ++ "}}}","\\p{A \\strong{displayed \\GAT{" ++ G ++ "}-algebra} ", "(over a \\GAT{" ++ G ++ "}-algebra #{" ++ (if topnames.length < 4 then String.intercalate ", " (topnames.map identFormat) else String.intercalate "," (List.take 3 (topnames.map identFormat)) ++ ",\\ldots") ++ "})","consists of","\\ul{"],["}","}"])
-| sfSect => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Sect{" ++ G ++ "}}}","\\p{A \\strong{section} of a displayed \\GAT{" ++ G ++ "}-algebra #{" ++ (if topnames.length < 4 then String.intercalate ", " (topnames.map (λ s => identFormat s ++ "\\dalg")) else String.intercalate ", " (List.take 3 (topnames.map (λ s => identFormat s ++ "\\dalg"))) ++ ",\\ldots") ++ "}", "(over a \\GAT{" ++ G ++ "}-algebra #{" ++ (if topnames.length < 4 then String.intercalate ", " (topnames.map identFormat) else String.intercalate "," (List.take 3 (topnames.map identFormat)) ++ ",\\ldots") ++ "})","consists of","\\ul{"],["}","}"])
-| sfHom => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Hom{" ++ G ++ "}}}","\\p{A \\strong{homomorphism} of \\GAT{" ++ G ++ "}-algebras (from (#{" ++ (if topnames.length < 4 then String.intercalate ", " (topnames.map (λ s => identFormat s ++ "_0")) else String.intercalate ", " (List.take 3 (topnames.map (λ s => identFormat s ++ "_0"))) ++ ",\\ldots") ++ "}) to (#{" ++ (if topnames.length < 4 then String.intercalate ", " (topnames.map (λ s => identFormat s ++ "_1")) else String.intercalate "," (List.take 3 (topnames.map (λ s => identFormat s ++ "_1"))) ++ ",\\ldots") ++ "}))","consists of","\\ul{"],["}","}"])
+| sfDalg => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\DAlg{" ++ G ++ "}}}","\\p{A \\strong{displayed \\GAT{" ++ G ++ "}-algebra} ", "(over a \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt identFormat topnames ++ ")","consists of","\\ul{"],["}","}"])
+| sfSect => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Sect{" ++ G ++ "}}}","\\p{A \\strong{section} of a displayed \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt (λ s => identFormat s ++ "\\dalg") topnames ++ " (over a \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt identFormat topnames ++ ")","consists of","\\ul{"],["}","}"])
+| sfHom => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Hom{" ++ G ++ "}}}","\\p{A \\strong{homomorphism} of \\GAT{" ++ G ++ "}-algebras (from " ++ inlineAlgFmt (λ s => identFormat s ++ "_0") topnames ++ " to " ++ inlineAlgFmt (λ s => identFormat s ++ "_1") topnames ++ ")","consists of","\\ul{"],["}","}"])
 | _ => ([],[])
 
 
