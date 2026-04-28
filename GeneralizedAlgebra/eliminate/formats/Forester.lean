c6ad3f42741c9ca0dd65ff7e91db2f535bda9285
Jacob Neumann <jacobneumann21@gmail.com>
2026-04-28 18:22:58 +0000

    Cleaned up Forester presentation, particularly with line breaks

--- GeneralizedAlgebra/eliminate/formats/Forester.lean
+++ GeneralizedAlgebra/eliminate/formats/Forester.lean
@@ -1,25 +1,64 @@
 import GeneralizedAlgebra.eliminate.formats.StringFormat
 
 
+def charSFIdentFormat : Char → String
+| 'Γ' => "\\Gamma"
+| 'Δ' => "\\Delta"
+| 'Θ' => "\\Theta"
+| 'Ξ' => "\\Xi"
+| 'γ' => "\\gamma"
+| 'ϑ' => "\\vartheta"
+| 'σ' => "\\sigma"
+| 'δ' => "\\delta"
+| 'ε' => "\\epsilon"
+| 'η' => "\\eta"
+| 'β' => "\\beta"
+| 'π' => "\\pi"
+| '_' => "\\_"
+| '(' => "\\lparen{}"
+| ')' => "\\rparen{}"
+| '₀' => "_0"
+| '₁' => "_1"
+| '₂' => "_2"
+| c => "\\mathsf{" ++ [c].asString ++ "}"
+
+def charIdentFormat : Char → String
+| 'Γ' => "\\Gamma"
+| 'Δ' => "\\Delta"
+| 'Θ' => "\\Theta"
+| 'Ξ' => "\\Xi"
+| 'γ' => "\\gamma"
+| 'ϑ' => "\\vartheta"
+| 'σ' => "\\sigma"
+| 'δ' => "\\delta"
+| 'ε' => "\\epsilon"
+| 'η' => "\\eta"
+| 'β' => "\\beta"
+| 'π' => "\\pi"
+-- | '_' => "\\_"
+| '(' => "\\lparen{}"
+| ')' => "\\rparen{}"
+| '₀' => "_0"
+| '₁' => "_1"
+| '₂' => "_2"
+| c => [c].asString
 
 
-def identFormat : String → String
-| "Γ" => "#{\\Gamma}"
-| "Δ" => "#{\\Delta}"
-| "Θ" => "#{\\Theta}"
-| "Ξ" => "#{\\Xi}"
-| "γ" => "#{\\gamma}"
-| "ϑ" => "#{\\vartheta}"
-| "σ" => "#{\\sigma}"
-| "δ" => "#{\\delta}"
-| "ε" => "#{\\epsilon}"
-| "η" => "#{\\eta}"
-| "π" => "#{\\pi}"
-| "π₀" => "#{\\pi_0}"
-| "π₁" => "#{\\pi_1}"
-| "π₂" => "#{\\pi_2}"
-| "ηε" => "#{\\eta\\epsilon}"
-| s => "\\mathsf{" ++ s ++ "}"
+
+def strCombine (s1 s2 : String) : String := match (s1.toList,s2.toList) with
+| ('\\'::'m'::'a'::'t'::'h'::'s'::'f'::'{'::rest1,'\\'::'m'::'a'::'t'::'h'::'s'::'f'::'{'::rest2) => "\\mathsf{" ++ rest1.dropLast.asString ++ rest2.asString
+| _ => s1 ++ s2
+
+def spaceIntercal (s1 s2 : String) : String := match (s1.toList.reverse,s2.toList) with
+| (_,'\\'::'t'::'o'::_) => s1 ++ " " ++ s2
+| (_,'\\'::'c'::'o'::'l'::'o'::'n'::_) => s1 ++ " " ++ s2
+| ('n'::'o'::'l'::'o'::'c'::'\\'::_,_) => s1 ++ " " ++ s2
+| ('o'::'t'::'\\'::_,_) => s1 ++ " " ++ s2
+| (_,[]) => s1
+| ([],_) => s2
+| _ => s1 ++ "\\;" ++ s2
+
+def identFormat (s : String) : String := List.foldr strCombine "" (s.toList.map charSFIdentFormat)
 
 open sfDecor
 
@@ -32,7 +71,7 @@ def forDecorate (s : String) : sfDecor → String
 | sfSect => s ++ "\\sect"
 | sfZero => s ++ "_0"
 
-def liFormat s := "\\li{#{" ++ s ++ "}}"
+def liFormat (s : String) := "\\li{#{" ++ List.foldr strCombine "" (s.toList.map charIdentFormat) ++ "}}"
 
 def forFormatLine (s : String) : sfDecor → String
 | sfId => liFormat s
@@ -47,17 +86,21 @@ def inlineAlgFmt : (String → String) → List String → String
 | f,l => "#{(" ++ String.intercalate ", " (List.map f (l.take 3)) ++ ", \\ldots)}"
 
 def forFormatWrapping (G _ : String) (topnames : List String) : sfDecor → List String × List String
-| sfId => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\mathfrak{" ++ G ++ "}}}","\\p{The GAT #{\\GAT{" ++ G ++ "}} is given by the signature","\\<html:ul>[style]{list-style-type:'▷  ';list-style-position: inside;}{","\\<html:li>[style]{list-style-type:'◇';list-style-position: outside;}{}"],["}","}"])
-| sfAlg => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Alg{" ++ G ++ "}}}","\\p{A \\strong{\\GAT{" ++ G ++ "}-algebra} consists of","\\ul{"],["}","}"])
-| sfDalg => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\DAlg{" ++ G ++ "}}}","\\p{A \\strong{displayed \\GAT{" ++ G ++ "}-algebra} ", "(over a \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt identFormat topnames ++ ")","consists of","\\ul{"],["}","}"])
-| sfSect => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Sect{" ++ G ++ "}}}","\\p{A \\strong{section} of a displayed \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt (λ s => identFormat s ++ "\\dalg") topnames ++ " (over a \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt identFormat topnames ++ ")","consists of","\\ul{"],["}","}"])
-| sfHom => (["\\taxon{Definition}","","\\import{index}","\\title{#{\\Hom{" ++ G ++ "}}}","\\p{A \\strong{homomorphism} of \\GAT{" ++ G ++ "}-algebras (from " ++ inlineAlgFmt (λ s => identFormat s ++ "_0") topnames ++ " to " ++ inlineAlgFmt (λ s => identFormat s ++ "_1") topnames ++ ")","consists of","\\ul{"],["}","}"])
+| sfId => (["\\p{The GAT #{\\GAT{" ++ G ++ "}} is given by the signature","\\<html:ul>[style]{list-style-type:'▷  ';list-style-position: inside;}{","\\<html:li>[style]{list-style-type:'◇';list-style-position: outside;}{}"],["}","}"])
+| sfAlg => (["\\p{A \\strong{\\GAT{" ++ G ++ "}-algebra} consists of","\\ul{"],["}","}"])
+| sfDalg => (["\\p{A \\strong{displayed \\GAT{" ++ G ++ "}-algebra} ", "(over a \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt identFormat topnames ++ ")","consists of","\\ul{"],["}","}"])
+| sfSect => (["\\p{A \\strong{section} of a displayed \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt (λ s => identFormat s ++ "\\dalg") topnames ++ " (over a \\GAT{" ++ G ++ "}-algebra " ++ inlineAlgFmt identFormat topnames ++ ")","consists of","\\ul{"],["}","}"])
+| sfHom => (["\\p{A \\strong{homomorphism} of \\GAT{" ++ G ++ "}-algebras (from " ++ inlineAlgFmt (λ s => identFormat s ++ "_0") topnames ++ " to " ++ inlineAlgFmt (λ s => identFormat s ++ "_1") topnames ++ ")","consists of","\\ul{"],["}","}"])
 | _ => ([],[])
 
 
+def insertBreak (s : String) : String := match s.toList with
+| '('::rest => "\\lparen{}}#{" ++ rest.asString
+| _ => s
+
 def forester : StringFormat := ⟨
     λ s => paren' "\\;" [s] ["\\;"],
-    String.intercalate "\\;",
+    List.foldl spaceIntercal "",
     forDecorate,
     identFormat,
     "\\Set",
@@ -68,12 +111,13 @@ def forester : StringFormat := ⟨
     "\\{",
     "\\}",
     "\\UU",
-    (String.intercalate "\\;" ["\\Pi",·,·]),
+    (List.foldl spaceIntercal "" ["\\Pi",·,insertBreak ·]),
     "\\Eq",
     "\\El",
     "@",
     "\\transp",
     "[wk]",
     forFormatWrapping,
-    forFormatLine
+    forFormatLine,
+    "} #{"
 ⟩
